bde294b809120e20a7c9b63a8407e822781b7bcd
Tom <tomvangrinsven92@gmail.com>
2019-10-29 15:37:01 +0100

    differentiate multple quizzes and their websockets

--- quizzer-server/app.js
+++ quizzer-server/app.js
@@ -36,32 +36,17 @@ const websocketServer = new ws.Server({ noServer: true });
 
 httpServer.on('upgrade', (req, networkSocket, head) => {
     sessionParser(req, {}, () => {
-        // The 'req' parameter contains the HTTP request that is for the upgrade
-        // request to the websocket protocol.
-        // We can refuse the upgrade request by returning from this function
-        // (and closing the networkconnection for this request)
-        // if (req.session.account === undefined) {
-        //     networkSocket.destroy();
-        //     return;
-        // }
 
         if(req.session.quizCode === undefined){
             console.log("Geen quizcode jammerdebammer");
             networkSocket.destroy();
             return;
         } else if (req.session.account === undefined && req.session.team === undefined) {
-            console.log("Geen account jammerdebammer");
+            console.log("Geen account of team jammerdebammer");
             networkSocket.destroy();
             return;
         }
 
-
-        //console.log('Session is parsed and we have a User!');
-
-        // Everything is fine. We tell the websocket server to
-        // initiate a new websocket connection for this request
-        // and emit a new connection event passing in the
-        // newly created websocket when the setup is complete
         websocketServer.handleUpgrade(req, networkSocket, head, newWebSocket => {
             websocketServer.emit('connection', newWebSocket, req);
         });
@@ -69,45 +54,17 @@ httpServer.on('upgrade', (req, networkSocket, head) => {
 });
 
 websocketServer.on('connection', (socket, req) => {
-
     socket.session = req.session;
 
-    console.log(socket.session.account);
+    // TODO: andere manier van het bijhouden van websockets <--> quiz;
 
-    socket.on('message', (message) => {
-        req.session.reload((err)=>{
-
-
-            console.log(req.session.team);
-
-            // if we don't call reload(), we'll get a old copy
-            // of the session, and won't see changes made by
-            // Express routes (like '/logout', above)
-            //if(err) { throw err };
-
-            // Here we can now use session parameters, because we called the sessionParser
-            // in the verifyClient function
-
-            // if( req.session.account == undefined ) {
-            //     // The session does not contain the name of a user, so this this client
-            //     // has probably logged out.
-            //     // We'll simply ignore any messages from this client.
-            //     console.log(`Ignoring message from logged out user: "${message}"` );
-            //     return;
-            // }
-
-            // req.session.messageCounter ++;
-            // //console.log(`${req.session.messageCounter}th WS message from ${req.session.userName}: "${message}"`);
-            //
-            // // broadcast this message to all connected browsers
-            // const outMessage = `[${req.session.messageCounter}]: ${message}`;
-            websocketServer.clients.forEach(function (client) {
-                client.send(JSON.stringify({message: "MESSAGE MAAT"}));
-            });
-            req.session.save()  // If we don't call save(), Express routes like '/logout' (above)
-                                // will not see the changes we make to the session in this socket code.
-        })
-    });
+    // if(websocketServer.quizClients === undefined){
+    //     websocketServer.quizClients = {};
+    // }
+    // if(websocketServer.quizClients[req.quiz.code] === undefined){
+    //     websocketServer.quizClients[req.quiz.code] = [];
+    // }
+    // websocketServer.quizClients[req.quiz.code].push(socket);
 });
 
 app.use('/quizzes', quizRouter);
@@ -118,7 +75,6 @@ app.use('/sessions', sessionsRouter);
 const dbName = "quizzer";
 
 httpServer.on('request', app);
-
 
 httpServer.listen(3000, function() {
     mongoose.connect(`mongodb://quizzer-user:supers3cretp4assword!@104.248.87.211:27017/${dbName}`,  {useNewUrlParser: true }, () => {
