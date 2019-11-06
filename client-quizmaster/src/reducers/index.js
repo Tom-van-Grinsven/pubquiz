f2f63d62158a6d73b89ff2fbf6c9a2b006f35eb5
dhelicopters <tomvangrinsven92@gmail.com>
2019-11-06 23:45:24 +0100

    final version v4

--- client-quizmaster/src/reducers/index.js
+++ client-quizmaster/src/reducers/index.js
@@ -8,7 +8,7 @@ import {categoryQuestionsReducer} from "./categoryQuestionsReducer";
 import {teamAnswersReducer} from "./teamAnswersReducer";
 import {activeQuestionReducer} from "./activeQuestionReducer";
 import {errorReducer} from "./errorReducer";
-import {webSocketReducer} from "./WebSocketReducer";
+import {webSocketReducer} from "./webSocketReducer";
 import {accountReducer} from "./accountReducer";
 
 const mainReducer = Redux.combineReducers({
